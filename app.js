62343b46ca1c04b36929ca3875b74f5167c30f59
syed afshana <syedafshana4812@gmail.com>
2021-10-17 22:39:24 +0530

    initial

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = 'mongodb+srv://affu:affu12@cluster0.rv1xb.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
